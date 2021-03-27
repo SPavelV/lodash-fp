df5ac4766111d55138798f08e3d844f27bbe7890
Pavel Solovev <p.soloviev@galament.com>
2021-03-27 18:12:11 +0400

    lodash flow & carring

--- hof.js
+++ hof.js
@@ -21,9 +21,24 @@ const divide = (a, b) => a / b;
 //   };
 // }
 
-const double = _.partial(multiple, 2);
+// const double = _.partial(multiple, 2);
 // console.log(`double(21) `, double(21));
 
 // const half = _.partialRight(divide, 2);
-const half = _.partial(divide, _, 2);
-console.log(`half(4) `, half(4));
+// const half = _.partial(divide, _, 2);
+// console.log(`half(4) `, half(4));
+
+// ====================
+
+// const curriedDivide = _.curry(divide);
+
+// console.log(`curriedDivide(10, 2) `, curriedDivide(10, 2));
+// console.log(`curriedDivide(10)(2) `, curriedDivide(10)(2));
+
+// ====================
+
+const notFlatArray = [1, 2, 3, [4, 5]]; // 15
+console.log(`sum`, _.sum(notFlatArray));
+
+const sumFlat = _.flow([_.concat, _.flattenDeep, _.sum]);
+console.log(`sumFlat(notFlatArray) `, sumFlat(notFlatArray));
