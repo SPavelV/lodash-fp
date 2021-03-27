b6ae93f941a5127e700d0f6288c9a62dcaea6946
Pavel Solovev <p.soloviev@galament.com>
2021-03-27 17:51:32 +0400

    partial function

--- hof.js
+++ hof.js
@@ -1,15 +1,29 @@
+const _ = require("lodash");
+
 const multiple = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 // console.log(`multiple(2,3) `, multiple(2, 3));
 
 // const double = (num) => multiple(num, 2);
 
-function partial(fn, ...fixed) {
-    return function(...args) {
-        return fn.apply(this, fixed.concat(args));
-    }
-}
+// function partial(fn, ...fixed) {
+//     return function(...args) {
+//         return fn.apply(this, fixed.concat(args));
+//     }
+// }
 
-const double = partial(multiple, 2);
+// function partial(fn) {
+//   const fixed = _.tail(_.toArray(arguments));
 
-console.log(`double(21) `, double(21));
+//   return function () {
+//     return fn.apply(this, _.concat(fixed, arguments));
+//   };
+// }
+
+const double = _.partial(multiple, 2);
+// console.log(`double(21) `, double(21));
+
+// const half = _.partialRight(divide, 2);
+const half = _.partial(divide, _, 2);
+console.log(`half(4) `, half(4));
