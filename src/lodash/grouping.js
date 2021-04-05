e1fe87d25ef6164359c6597337670625ddb1256d
Pavel Solovev <pashius2010@gmail.com>
2021-04-05 09:00:51 +0400

    grouping

--- src/lodash/grouping.js
+++ src/lodash/grouping.js
@@ -2,6 +2,12 @@ const _ = require("lodash");
 
 const users = [
   {
+    id: 3,
+    name: "Mike",
+    active: false,
+    likes: 30,
+  },
+  {
     id: 2,
     name: "Jack",
     active: false,
@@ -15,11 +21,19 @@ const users = [
   },
 ];
 
-const groupBy = (list, prop) => ({
-  prop: list.filter((item) =>
-    Object.values(item).some((propItem) => propItem === prop)
-  ),
-});
+const groupBy = (list, prop) => {
+  return list.reduce((aggregator, element) => {
+    console.log(
+      "qqq",
+      aggregator,
+      element,
+      element[prop],
+      aggregator[element[prop]]
+    );
+    (aggregator[element[prop]] = aggregator[element[prop]] || []).push(element);
+    return aggregator;
+  }, {});
+};
 
 const result = groupBy(users, "active");
 console.log(`result `, result);
