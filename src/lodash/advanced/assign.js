87b77bb814ae6c4cfab073bd4e868e5cc935e2d2
Pavel Solovev <pashius2010@gmail.com>
2021-04-09 07:37:58 +0400

    immutable data refresh

---
+++ src/lodash/advanced/assign.js
@@ -0,0 +1,12 @@
+const _ = require("lodash");
+
+const state = {
+  isLoading: false,
+  data: null,
+  error: null,
+};
+
+const newStateLodash = _.assign({}, state, { isLoading: true });
+console.log(`newStateLodash `, state, newStateLodash);
+const newState = Object.assign({}, state, { isLoading: true }); // native js
+// console.log("result", state, newState);
